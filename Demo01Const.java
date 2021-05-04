e2c1eba2d80c60dbb39f827a6d21dbddc0cd17b3
wangjong <2731473097@qq.com>
2021-05-04 22:17:01 +0800

    输出各种类型的练习

---
+++ Demo01Const.java
@@ -0,0 +1,25 @@
+public class Demo01Const {
+	public static void main(String [] args){
+		//字符串常量
+		System.out.println("ABC");
+		System.out.println("");
+		System.out.println("XYZ");
+		
+		//整数常量
+		System.out.println(30);
+		System.out.println(-500);
+		//浮点数常量
+		System.out.println(3.14);
+		System.out.println(-2.5);
+		//字符常量
+		System.out.println('A');
+		System.out.println('B');
+		//System.out.println('');单引号里有且仅有一个字符，不能没有东西
+		//Systeme.out.println('AA');单引号里有且仅有一个字符
+		//布尔类型
+		System.out.println(true);
+		System.out.println(false);
+		//空常量不能进行打印输出
+		//System.out.pritnln(null);这种写法是错误的；
+	}
+}
